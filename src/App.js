79dc7c49adf2b23ed7ef919fa0268d06158d5381
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-21 15:26:35 +0800

    added new folder structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
 import './styles/main.css';
 
-function App() {
+import MainNavigation from './components/MainNavigation';
+import LoginPage from './pages/LogIn';
+import SignUpPage from './pages/SignUp';
+import EventsPage from './pages/Events';
+import BookingsPage from './pages/Bookings';
+
+const App = () => {
   return (
-    <div className="p-6 max-w-sm max-w-md max-w-lg mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-      <div className="flex-shrink-0">
-        <img
-          className="h-12 w-12"
-          src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80"
-          alt="ChitChat Logo"
-        />
-      </div>
-      <div>
-        <div className="text-xl font-medium text-black">ChitChat</div>
-        <p className="text-gray-500">You have a new message!</p>
-      </div>
-    </div>
+    <Router>
+      <React.Fragment>
+        <MainNavigation />
+        <main></main>
+      </React.Fragment>
+    </Router>
   );
-}
+};
 
 export default App;

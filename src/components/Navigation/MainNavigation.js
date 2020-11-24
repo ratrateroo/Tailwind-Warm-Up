1003a181fe59af932609b225f70f9bd2b4c6262c
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-24 23:41:51 +0800

    trying out colors

--- src/components/Navigation/MainNavigation.js
+++ src/components/Navigation/MainNavigation.js
@@ -5,20 +5,32 @@ import { NavLink } from 'react-router-dom';
 const MainNavigation = () => {
   return (
     <header className="">
-      <div class="">
-      <nav className="">
-        <NavLink to="/events" className="">
+      <div class="container mx-auto bg-yellow-700 lg:w-auto">
+      <nav className="flex flex-row-reverse space-x-4 space-x-reverse items-center">
+        <div>
+          
+        <NavLink to="/events" className="bg-black-700 text-white">
+          Logo
+        </NavLink>
+        </div>
+        <div>
+
+        <NavLink to="/events" className="bg-black-700 text-white">
           Events
         </NavLink>
-        <NavLink to="/bookings" className="">
+        <NavLink to="/bookings" className="bg-black-700 text-white">
           Bookings
         </NavLink>
-        <NavLink to="/signup" className="">
+        </div>
+        <div>
+
+        <NavLink to="/signup" className="bg-black-700 text-white">
           Sign Up
         </NavLink>
-        <NavLink to="/login" className="">
+        <NavLink to="/login" className="bg-black-700 text-white">
           Log In
         </NavLink>
+        </div>
       </nav>
       </div>
     </header>

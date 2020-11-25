bf523ecccb224cbc435b2fd4e7d0ca0da8a33111
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-25 17:22:33 +0800

    aligning elements

--- src/components/Navigation/MainNavigation.js
+++ src/components/Navigation/MainNavigation.js
@@ -4,33 +4,31 @@ import { NavLink } from 'react-router-dom';
 
 const MainNavigation = () => {
   return (
-    <header className="">
-      <div class="container mx-auto bg-gray-900 bg-opacity-100  lg:w-auto">
-      <nav className="flex flex-row-reverse space-x-10 space-x-reverse items-center">
+    <header className="p-0">
+      <div class="flex items-center h-20 w-screen sm:bg-red-900 md:bg-green-900 lg:bg-blue-900 ">
+      <nav className="h-10  w-full flex space-x-10 sm:bg-red-500 md:bg-green-500 lg:bg-blue-500">
         <div>
           
-        <NavLink to="/events" className="bg-black-700 text-white">
+        <NavLink to="/events" className="bg-gray-700 text-white">
           Logo
         </NavLink>
         </div>
         <div>
 
-        <NavLink to="/events" className="bg-black-700 text-white">
+        <NavLink to="/events" className="bg-gray-700 text-white">
           Events
         </NavLink>
-        <NavLink to="/bookings" className="bg-black-700 text-white">
+        <NavLink to="/bookings" className="bg-gray-700 text-white">
           Bookings
         </NavLink>
-        <div className="md:bg-black-700 text-white">
-          TEXT
-        </div>
+        
         </div>
         <div>
 
-        <NavLink to="/signup" className="bg-black-700 text-white">
+        <NavLink to="/signup" className="bg-gray-700 text-white">
           Sign Up
         </NavLink>
-        <NavLink to="/login" className="bg-black-700 text-white">
+        <NavLink to="/login" className="bg-gray-700 text-white">
           Log In
         </NavLink>
         </div>

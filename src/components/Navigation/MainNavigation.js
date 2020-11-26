88bc2fd2302f76d57c26d3cf4091e8c38a515830
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-26 22:46:06 +0800

    aligned items correctly for events page

--- src/components/Navigation/MainNavigation.js
+++ src/components/Navigation/MainNavigation.js
@@ -3,38 +3,35 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const MainNavigation = () => {
-  return (
-    <header className="p-0">
-      <div class="flex items-center h-20 w-screen bg-yellow-900 sm:bg-red-900 md:bg-green-900 lg:bg-blue-900 xl:bg-pink-900 2xl:bg-indigo-900">
-      <nav className="h-10  w-full flex justify-between bg-yellow-500 sm:bg-red-500 md:bg-green-500 lg:bg-blue-500 xl:bg-pink-500 2xl:bg-indigo-500">
-        <div className="w-max  flex justify-center items-center mx-1 bg-gray-500">
-          
-        <NavLink to="/events" className="mx-5 bg-gray-700">
-         LOGO
-        </NavLink>
-        </div>
-        <div className="w-full flex justify-start items-center mx-1 bg-gray-500">
-
-        <NavLink to="/events" className="mx-5 bg-gray-700 text-white">
-          Events
-        </NavLink>
-        <NavLink to="/bookings" className="mx-5 bg-gray-700 text-white">
-          Bookings
-        </NavLink>
-        </div>
-        <div className="w-full flex justify-end items-center mx-1 bg-gray-500">
-
-        <NavLink to="/signup" className="mx-5 bg-gray-700 text-white">
-          Sign Up
-        </NavLink>
-        <NavLink to="/login" className="mx-5 bg-gray-700 text-white">
-          Log In
-        </NavLink>
-        </div>
-      </nav>
-      </div>
-    </header>
-  );
+	return (
+		<header className="p-0">
+			<div class="flex items-center h-20 w-screen bg-yellow-900 sm:bg-red-900 md:bg-green-900 lg:bg-blue-900 xl:bg-pink-900 2xl:bg-indigo-900">
+				<nav className="h-10  w-full flex justify-between bg-yellow-500 sm:bg-red-500 md:bg-green-500 lg:bg-blue-500 xl:bg-pink-500 2xl:bg-indigo-500">
+					<div className="flex justify-start items-center mx-1">
+						<NavLink to="/events" className="h-10 w-10 mx-3">
+							<img src="Logo.svg" className="" alt="Logo" />
+						</NavLink>
+					</div>
+					<div className="w-full flex justify-start items-center mx-1">
+						<NavLink to="/events" className="mx-5 text-white">
+							Events
+						</NavLink>
+						<NavLink to="/bookings" className="mx-5 text-white">
+							Bookings
+						</NavLink>
+					</div>
+					<div className="w-full flex justify-end items-center mx-1">
+						<NavLink to="/signup" className="mx-5 text-white">
+							Sign Up
+						</NavLink>
+						<NavLink to="/login" className="mx-5 mr-10 text-white">
+							Log In
+						</NavLink>
+					</div>
+				</nav>
+			</div>
+		</header>
+	);
 };
 
 export default MainNavigation;
--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -3,10 +3,71 @@ import React from 'react';
 const EventsPage = () => {
 	return (
 		<div>
-			<h1>Events Page</h1>
-			<div className="h-screen w-screen bg-yellow-300 sm:bg-red-300 md:bg-green-300 lg:bg-blue-300 xl:bg-pink-300 2xl:bg-indigo-300">
-				<div>
-					<img src="Logo.svg" class="w-max" />
+			<h1 className="p-5 text-2xl font-bold text-black">Events Page</h1>
+			<div className="h-screen w-full flex justify-center bg-yellow-300 sm:bg-red-300 md:bg-green-300 lg:bg-blue-300 xl:bg-pink-300 2xl:bg-indigo-300">
+				<div className="w-full p-10  flex flex-col justify-start bg-gray-100">
+					<div className="w-3/4 mx-auto my-5 p-5 flex justify-between rounded-lg bg-yellow-100">
+						<div className="">
+							<div className="py-2">
+								<h1 class="text-2xl font-bold text-yellow-900">
+									Pyrolympics
+								</h1>
+							</div>
+							<div className="py-2 flex justify-between">
+								<div>
+									<p className="text-xl font-bold text-yellow-600">
+										&#8369; 1000.00
+									</p>
+								</div>
+								<div>
+									<p className="text-xl mx-5 font-bold text-yellow-600">
+										-
+									</p>
+								</div>
+								<div>
+									<p className="text-xl font-bold text-yellow-600">
+										January 1, 2021
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="h-full flex items-center">
+							<button className="p-3 rounded text-white font-bold bg-yellow-500">
+								View Details
+							</button>
+						</div>
+					</div>
+					<div className="w-3/4 mx-auto my-5 p-5 flex justify-between rounded-lg bg-yellow-100">
+						<div className="">
+							<div className="py-2">
+								<h1 class="text-2xl font-bold text-yellow-900">
+									Pyrolympics
+								</h1>
+							</div>
+							<div className="py-2 flex justify-between">
+								<div>
+									<p className="text-xl font-bold text-yellow-600">
+										&#8369; 1000.00
+									</p>
+								</div>
+								<div>
+									<p className="text-xl mx-5 font-bold text-yellow-600">
+										-
+									</p>
+								</div>
+								<div>
+									<p className="text-xl font-bold text-yellow-600">
+										January 1, 2021
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="h-full flex items-center">
+							<button className="p-3 rounded text-white font-bold bg-yellow-500">
+								View Details
+							</button>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>

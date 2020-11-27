7a9d5028222c51bdc55b9bc8d667d4872dfba770
Mark Leo Tarectecan <ratrateroo@gmail.com>
2020-11-27 23:01:03 +0800

    added active classnames

--- src/components/Navigation/MainNavigation.js
+++ src/components/Navigation/MainNavigation.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 const MainNavigation = () => {
 	return (
 		<header className="p-0">
-			<div class="flex items-center h-20 w-screen bg-yellow-900 sm:bg-red-900 md:bg-green-900 lg:bg-blue-900 xl:bg-pink-900 2xl:bg-indigo-900">
+			<div class="flex items-center py-2.5 w-screen bg-yellow-900 sm:bg-red-900 md:bg-green-900 lg:bg-blue-900 xl:bg-pink-900 2xl:bg-indigo-900">
 				<nav className="h-10  w-full flex justify-between bg-yellow-500 sm:bg-red-500 md:bg-green-500 lg:bg-blue-500 xl:bg-pink-500 2xl:bg-indigo-500">
 					<div className="flex justify-start items-center mx-1">
 						<NavLink to="/events" className="h-10 w-10 mx-3">
@@ -13,18 +13,34 @@ const MainNavigation = () => {
 						</NavLink>
 					</div>
 					<div className="w-full flex justify-start items-center mx-1">
-						<NavLink to="/events" className="mx-5 text-white">
+						<NavLink
+							exact
+							to="/events"
+							className="mx-5 text-white"
+							activeClassName="border-b-2 border-white">
 							Events
 						</NavLink>
-						<NavLink to="/bookings" className="mx-5 text-white">
+						<NavLink
+							to="/bookings"
+							className="mx-5 text-white"
+							activeClassName="border-b-2 border-white"
+							exact>
 							Bookings
 						</NavLink>
 					</div>
 					<div className="w-full flex justify-end items-center mx-1">
-						<NavLink to="/signup" className="mx-5 text-white">
+						<NavLink
+							to="/signup"
+							className="mx-5 text-white"
+							activeClassName="border-b-2 border-white"
+							exact>
 							Sign Up
 						</NavLink>
-						<NavLink to="/login" className="mx-5 mr-10 text-white">
+						<NavLink
+							to="/login"
+							className="mx-5 mr-10 text-white"
+							activeClassName="border-b-2 border-white"
+							exact>
 							Log In
 						</NavLink>
 					</div>

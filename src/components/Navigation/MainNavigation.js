a4e11cc7fb059713faf638a72f7c0e68d2e57437
Mark <ratrateroo@gmail.com>
2020-12-17 22:49:37 +0800

    updated navigation

--- src/components/Navigation/MainNavigation.js
+++ src/components/Navigation/MainNavigation.js
@@ -1,39 +1,36 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const MainNavigation = () => {
-  return (
-    <header>
-      <div>
-        <h1>LOGO</h1>
-      </div>
-      <nav class="hidden md:flex space-x-10">
-        <a
-          href="#"
-          class="text-base font-medium text-gray-500 hover:text-gray-900"
-        >
-          Events
-        </a>
-        <a
-          href="#"
-          class="text-base font-medium text-gray-500 hover:text-gray-900"
-        >
-          Bookings
-        </a>
-        <a
-          href="#"
-          class="text-base font-medium text-gray-500 hover:text-gray-900"
-        >
-          Sign Up
-        </a>
-        <a
-          href="#"
-          class="text-base font-medium text-gray-500 hover:text-gray-900"
-        >
-          Log In
-        </a>
-      </nav>
-    </header>
-  );
+	return (
+		<header>
+			<div>
+				<h1>LOGO</h1>
+			</div>
+			<nav class="hidden md:flex space-x-10">
+				<NavLink
+					to="/events"
+					class="text-base font-medium text-gray-500 hover:text-gray-900">
+					Events
+				</NavLink>
+				<NavLink
+					to="/bookings"
+					class="text-base font-medium text-gray-500 hover:text-gray-900">
+					Bookings
+				</NavLink>
+				<NavLink
+					to="/signup"
+					class="text-base font-medium text-gray-500 hover:text-gray-900">
+					Sign Up
+				</NavLink>
+				<NavLink
+					to="/login"
+					class="text-base font-medium text-gray-500 hover:text-gray-900">
+					Log In
+				</NavLink>
+			</nav>
+		</header>
+	);
 };
 
 export default MainNavigation;

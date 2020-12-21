2d3cf78899e64b21a1400e2d7e07db76e607c4b1
Mark <ratrateroo@gmail.com>
2020-12-21 21:50:44 +0800

    added profile page

---
+++ src/pages/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BookingsPage = () => {
+	return (
+		<div>
+			<h1 className="p-5 text-2xl font-bold text-black">Profile</h1>
+			<div className="h-screen w-full flex justify-center bg-yellow-300 sm:bg-red-300 md:bg-green-300 lg:bg-blue-300 xl:bg-pink-300 2xl:bg-indigo-300">
+				<div className="w-full p-10  flex flex-col justify-start bg-gray-100"></div>
+			</div>
+		</div>
+	);
+};
+
+export default BookingsPage;
